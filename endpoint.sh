c1326498bbe40b13c7200d72a8580ceb8dbd3317
Mohameddiv <84381702+Mohameddiv@users.noreply.github.com>
2024-07-27 15:16:34 +0000

    Update endpoint.sh

--- endpoint.sh
+++ endpoint.sh
@@ -4,6 +4,7 @@ python3 $HOME/endpointCollect/waymore/waymore/waymore.py -i $1  -mode U -oU $HOM
 #cat $HOME/.config/waymore/results/$1/waymore.txt|anew $HOME/endpointCollect/endpoints/$1.txt && rm -rf $HOME/.config/waymore/results/$1/waymore.txt
 gau $1 |anew $HOME/endpointCollect/endpoints/$1.txt
 waybackurls $1|anew $HOME/endpointCollect/endpoints/$1.txt
+echo "$1" |  (gauplus || hakrawler)| grep -Ev "\.(jpeg|jpg|png|ico|woff|svg|css|ico|woff|ttf)$" |anew $HOME/endpointCollect/endpoints/$1.txt
 python3 $HOME/endpointCollect/crawler/crawler.py -n All -t $1 >>ali.txt
 cat ali.txt|grep "URL"|cut -d " " -f3 >>$HOME/endpointCollect/endpoints/$1.txt
 rm ali.txt
